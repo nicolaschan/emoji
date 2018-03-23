7e01e298ee81e30a8552f2e51893cfe376afe967
Nicolas Chan <nicolas@nicolaschan.com>
2018-03-22 17:20:08 -0700

    Performance improvements using arrays

--- RandomList.hs
+++ RandomList.hs
@@ -1,8 +1,14 @@
-module RandomList (randomize) where
+module RandomList (randomize, randomizeArray) where
 
-import System.Random
+import Data.Array (Array, bounds, (!))
+import System.Random (getStdGen, randomRs)
 
 randomize :: [a] -> IO [a]
 randomize xs = do
   g <- getStdGen
   return $ map (xs !!) (randomRs (0, length xs - 1) g)
+
+randomizeArray :: Array Int e -> IO [e]
+randomizeArray arr = do
+  g <- getStdGen
+  return $ map (arr !) (randomRs (0, (snd . bounds) arr - 1) g)
